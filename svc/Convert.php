e1204dfae451e7fad6a9bf596494df0d418f4cbe
Matt Dizak <matt.dizak@gmail.com>
2021-11-22 15:34:42 -0800

    dev update

--- svc/Convert.php
+++ svc/Convert.php
@@ -1,5 +1,5 @@
 <?php
-delcare(strict_types = 1);
+declare(strict_types=1);
 
 namespace Apex\Svc;
 
--- svc/Queue.php
+++ svc/Queue.php
@@ -3,6 +3,9 @@ declare(strict_types = 1);
 
 namespace Apex\Svc;
 
+use Apex\App\Sys\TaskModel;
+use DateTime;
+
 /**
  * Queue
  */
@@ -12,16 +15,45 @@ class Queue
     /**
      * Add
      */
-    public function add(string $class_name, array $params = []):void
+    public function add(string $class_name, array $params = [], ?DateTime $execute_time = null):TaskModel
     {
 
-        // Add to database
-        $this->db->insert('internal_tasks', [
+        // Get insert vars
+        $insert_vars = [
+            'is_onetime' => true,
             'class_name' => $class_name,
-            'data' => json_encode($data)
-        ]);
+            'data' => json_encode($params)
+        ];
 
+        // Add execute time to insert vars
+        if ($execute_time !== null) {
+            $insert_vars['execute_time'] = $execute_time->format('Y-m-d H:i:s');
+        }
+
+        // Add to database
+        $task = TaskModel::insert($insert_vars);
+
+        // Return
+        return $task;
     }
+
+    /**
+     * List
+     */
+    public function list():?ModelIterator
+    {
+        return TaskModel::where('is_onetime = %b ORDER BY execute_time', true);
+    }
+
+    /**
+     * Purge
+     */
+    public function purge():void
+    {
+        TaskModel::delete(['is_onetime' => true]);
+    }
+
+
 
 }
 
